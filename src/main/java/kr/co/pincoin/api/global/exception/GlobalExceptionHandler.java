2b2bf64d810f9d6acee27e3a187ef8b499bdd8b5
mairoo <jonghwa@pincoin.co.kr>
2024-12-09 13:23:12 +0900

    handleBadRequestException

--- src/main/java/kr/co/pincoin/api/global/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/pincoin/api/global/exception/GlobalExceptionHandler.java
@@ -83,6 +83,7 @@ public class GlobalExceptionHandler {
                 validationErrors));
   }
 
+  /** 잘못된 요청 파라미터 예외 처리  */
   @ExceptionHandler(BadRequestException.class)
   protected ResponseEntity<ErrorResponse> handleBadRequestException(
       BadRequestException e, HttpServletRequest request) {
