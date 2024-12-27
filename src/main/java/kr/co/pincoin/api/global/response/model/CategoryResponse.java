63b1a9d762b392ee3d7312f581a158acc90623c9
jonghwa <jonghwa@pincoin.co.kr>
2024-12-27 10:55:24 +0900

    json properties

--- src/main/java/kr/co/pincoin/api/global/response/model/CategoryResponse.java
+++ src/main/java/kr/co/pincoin/api/global/response/model/CategoryResponse.java
@@ -1,6 +1,7 @@
 package kr.co.pincoin.api.global.response.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import kr.co.pincoin.api.domain.shop.model.product.Category;
 import kr.co.pincoin.api.domain.shop.model.product.CategoryDetached;
@@ -14,28 +15,40 @@ import lombok.Getter;
 @Builder(access = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CategoryResponse {
+  @JsonProperty("id")
   private final Long id;
 
+  @JsonProperty("title")
   private final String title;
 
+  @JsonProperty("slug")
   private final String slug;
 
+  @JsonProperty("thumbnail")
   private final String thumbnail;
 
+  @JsonProperty("description")
   private final String description;
 
+  @JsonProperty("description1")
   private final String description1;
 
+  @JsonProperty("discountRate")
   private final BigDecimal discountRate;
 
+  @JsonProperty("pg")
   private final Boolean pg;
 
+  @JsonProperty("pgDiscountRate")
   private final BigDecimal pgDiscountRate;
 
+  @JsonProperty("naverSearchTag")
   private final String naverSearchTag;
 
+  @JsonProperty("naverBrandName")
   private final String naverBrandName;
 
+  @JsonProperty("naverMakerName")
   private final String naverMakerName;
 
   // 도메인 모델 객체에서 응답 객체 초기화
--- src/main/java/kr/co/pincoin/api/global/response/model/VoucherResponse.java
+++ src/main/java/kr/co/pincoin/api/global/response/model/VoucherResponse.java
@@ -1,6 +1,7 @@
 package kr.co.pincoin.api.global.response.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import kr.co.pincoin.api.domain.shop.model.product.Voucher;
 import kr.co.pincoin.api.domain.shop.model.product.enums.VoucherStatus;
@@ -14,20 +15,29 @@ import lombok.Getter;
 @Builder(access = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class VoucherResponse {
+
+  @JsonProperty("id")
   private final Long id;
 
+  @JsonProperty("code")
   private final String code;
 
+  @JsonProperty("remarks")
   private final String remarks;
 
+  @JsonProperty("status")
   private final VoucherStatus status;
 
+  @JsonProperty("productId")
   private final Long productId;
 
+  @JsonProperty("productName")
   private final String productName;
 
+  @JsonProperty("created")
   private final LocalDateTime created;
 
+  @JsonProperty("modified")
   private final LocalDateTime modified;
 
   public static VoucherResponse from(Voucher voucher) {
