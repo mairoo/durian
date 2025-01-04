0853466924142df22f26506a761bed54229ab2e9
mairoo <jonghwa@pincoin.co.kr>
2025-01-04 22:08:08 +0900

    remove static import

--- src/main/java/kr/co/pincoin/api/infra/shop/repository/order/OrderPaymentQueryRepositoryImpl.java
+++ src/main/java/kr/co/pincoin/api/infra/shop/repository/order/OrderPaymentQueryRepositoryImpl.java
@@ -1,39 +1,39 @@
 package kr.co.pincoin.api.infra.shop.repository.order;
 
-import static kr.co.pincoin.api.infra.shop.entity.order.QOrderPaymentEntity.orderPaymentEntity;
-
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import kr.co.pincoin.api.domain.shop.model.order.OrderPaymentDetached;
+import kr.co.pincoin.api.infra.shop.entity.order.QOrderPaymentEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderPaymentQueryRepositoryImpl implements OrderPaymentQueryRepository {
-
   private final JPAQueryFactory queryFactory;
 
   @Override
   public List<OrderPaymentDetached> findOrderPaymentsDetachedByOrderId(Long orderId) {
+    QOrderPaymentEntity orderPayment = QOrderPaymentEntity.orderPaymentEntity;
+
     return queryFactory
         .select(
             Projections.constructor(
                 OrderPaymentDetached.class,
-                orderPaymentEntity.id,
-                orderPaymentEntity.account,
-                orderPaymentEntity.amount,
-                orderPaymentEntity.balance,
-                orderPaymentEntity.received,
-                orderPaymentEntity.order.id,
-                orderPaymentEntity.order.orderNo,
-                orderPaymentEntity.created,
-                orderPaymentEntity.modified,
-                orderPaymentEntity.isRemoved))
-        .from(orderPaymentEntity)
-        .join(orderPaymentEntity.order)
-        .where(orderPaymentEntity.order.id.eq(orderId))
+                orderPayment.id,
+                orderPayment.account,
+                orderPayment.amount,
+                orderPayment.balance,
+                orderPayment.received,
+                orderPayment.order.id,
+                orderPayment.order.orderNo,
+                orderPayment.created,
+                orderPayment.modified,
+                orderPayment.isRemoved))
+        .from(orderPayment)
+        .join(orderPayment.order)
+        .where(orderPayment.order.id.eq(orderId))
         .fetch();
   }
 }
