d435b3f7f6030b98f576687f4c129f86ce905594
mairoo <jonghwa@pincoin.co.kr>
2025-01-05 00:59:34 +0900

    cart repository bug fix

--- src/main/java/kr/co/pincoin/api/domain/shop/repository/order/CartRepository.java
+++ src/main/java/kr/co/pincoin/api/domain/shop/repository/order/CartRepository.java
@@ -3,10 +3,9 @@ package kr.co.pincoin.api.domain.shop.repository.order;
 import java.util.Optional;
 import kr.co.pincoin.api.domain.auth.model.user.User;
 import kr.co.pincoin.api.domain.shop.model.order.Cart;
-import org.springframework.data.repository.query.Param;
 
 public interface CartRepository {
   Cart save(Cart cart);
 
-  Optional<Cart> findByUser(@Param("user") User user);
+  Optional<Cart> findByUser(User user);
 }
--- src/main/java/kr/co/pincoin/api/infra/shop/repository/order/CartJpaRepository.java
+++ src/main/java/kr/co/pincoin/api/infra/shop/repository/order/CartJpaRepository.java
@@ -5,8 +5,11 @@ import kr.co.pincoin.api.infra.auth.entity.user.UserEntity;
 import kr.co.pincoin.api.infra.shop.entity.order.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartJpaRepository extends JpaRepository<CartEntity, Long> {
   @Query("SELECT c FROM CartEntity c JOIN FETCH c.user WHERE c.user = :user")
-  Optional<CartEntity> findByUser(UserEntity userEntity);
+  Optional<CartEntity> findByUser(@Param("user") UserEntity userEntity);
 }
